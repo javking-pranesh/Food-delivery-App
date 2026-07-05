8f9c681eeee75f2f084bdaf5057d1031a59ab52e
javking-pranesh <javkingpranesh18@gmail.com>
2026-07-05 18:58:40 +0530

    Created Footer and App download Components

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut iusto excepturi totam, exercitationem delectus quas vitae quaerat doloremque error quis consequuntur voluptatem fugit nam ab deserunt aspernatur asperiores iure temporibus itaque .</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>TOMATO</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+
+        </div>
+        <div className="footer-content-right">
+            <h2>Get in Touch</h2>
+            <ul>
+                <li>9042585083</li>
+                <li>javkingpranesh18@gmail.com</li>
+            </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">Copyright 2026 &copy; Tomato.com All right Reserved. </p>
+    </div>
+  )
+}
+
+export default Footer
