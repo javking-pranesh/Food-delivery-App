25f163ce6360da65ef143d1e56a936c1e9662a12
javking-pranesh <javkingpranesh18@gmail.com>
2026-07-07 21:59:00 +0530

    Created addFood end point feauture

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 
 import dotenv from "dotenv";
+import foodRouter from "./routes/foodRoute.js";
+
 dotenv.config();
 
 
 // app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json())
@@ -17,6 +19,10 @@ app.use(cors())
 //db Connection
 
 connectDB();
+
+// api endpoints
+app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
 
 
 app.get("/", (req,res)=>{
