a5724e2fdbae4e1dcab9f2a43da6823042403596
javking-pranesh <javkingpranesh18@gmail.com>
2026-07-07 20:40:15 +0530

    Moved password to .env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
+import { connectDB } from "./config/db.js"
 
+import dotenv from "dotenv";
+dotenv.config();
 
 
 // app config
@@ -11,6 +14,11 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 
+//db Connection
+
+connectDB();
+
+
 app.get("/", (req,res)=>{
     res.send("API Working")
 })
@@ -18,3 +26,4 @@ app.get("/", (req,res)=>{
 app.listen(port,()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
+
