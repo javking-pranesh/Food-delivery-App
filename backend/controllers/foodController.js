91b72e4f293536ecba0f0be82cf4f5e068a84693
javking-pranesh <javkingpranesh18@gmail.com>
2026-07-07 22:26:42 +0530

    Created deleteFood endpoint

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -38,5 +38,20 @@ const listFood = async (req,res)=>{
     }
 }
 
+//remove food items 
 
-export { addFood, listFood };
+const removeFood = async (req,res) =>{
+    try{
+        const food = await foodModel.findById(req.body.id)
+        fs.unlink(`uploads/${food.image}`,()=>{
+
+        })
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success: true, message: "Food removed"})
+    }catch(error){
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+export { addFood, listFood,removeFood };
