6a1789099907bb66631e4a801200fb4086d876a2
javking-pranesh <javkingpranesh18@gmail.com>
2026-07-08 09:45:11 +0530

    Created Router and Navlink feauture

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr className='ruler'/>
       <div className='app-content'>
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}></Route>
+          <Route path='/list' element={<List/>}></Route>
+          <Route path='/orders' element={<Orders/>}></Route>
+        </Routes>
       </div>
     </div>
   )
